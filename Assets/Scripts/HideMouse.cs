265a8aedd0d6fb97ca905904355c758399736466
Brian <nairb725@gmail.com>
2024-02-02 23:08:25 +0100

    oui je continue

--- Assets/Scripts/HideMouse.cs
+++ Assets/Scripts/HideMouse.cs
@@ -6,12 +6,6 @@ public class HideMouse : MonoBehaviour {
     void Start()
     {
         // Lock and hide the mouse cursor
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-    }
-
-    void Update()
-    {
-        // You can add additional logic here if needed
     }
 }
